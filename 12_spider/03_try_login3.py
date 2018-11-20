504012454364e7bbfa910ca8017c55e44e266941
yangfan <171690224@qq.com>
2018-11-20 14:54:42 +0800

    hide the personal message in file

--- 12_spider/03_try_login3.py
+++ 12_spider/03_try_login3.py
@@ -10,20 +10,10 @@ post_url = "https://passport.zcool.com.cn/login_jsonp_active.do"
 headers = {
     'User-Agent': "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1"}
 
-datastr = {
-    "jsonpCallback": "jQuery19104615771324313176_1542545164725",
-    "appId": "1006",
-    "username": "18872729852",
-    "password": "08cba4c74028157bb51c3f33b7d77a59a951578c4b7cf4cceb02dc5e86c834a2",
-    "autoLogin": "0",
-    "code": "",
-    "service": "https://www.zcool.com.cn/u/17655780",
-    "appLogin": "https://www.zcool.com.cn/login_cb",
-    "_": "1542545164727"}
 
 post_data = {
-    "username": "18872729852",
-    "password": "YANGFAN0711"
+    "username": "***************",
+    "password": "****************"
 }
 
 session.post(post_url, headers=headers, data=post_data)
